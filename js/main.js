11eb2983e20b5f544babf3be256d87d9e5a4229c
Grzegorz Jeremenko <grzesiula002@gmail.com>
2020-03-07 22:36:35 +0100

    Add scroing mechanics

--- js/main.js
+++ js/main.js
@@ -1,66 +1,157 @@
-let wordsQuest = [], wordsAnswer = [];
+let words = [], wordNum = 0;
+let wordInput = [];
+let progressTo = 0, progressNow = 0, letterMis = "#2ecc71";
+let point = 0, mis = 0;
+
+let correctBar = 100, misBar = 0;
+
+setInterval(progressUpdate, 2);
 
 document.querySelector("#fileInput").addEventListener('change', function() {
     let all_files = this.files;
     
 	if(all_files.length == 0) {
-		console.log('Error : No file selected');
+		alert('No file selected');
 		return;
 	}
 	let file = all_files[0];
 
 	let allowed_types = ['text/plain'];
 	if(allowed_types.indexOf(file.type) == -1) {
-		console.log('Error : Incorrect file type');
+		alert('Incorrect file type');
 		return;
 	}
 
 	let max_size_allowed = 1024*1024;
 	if(file.size > max_size_allowed) {
-		console.log('Error : Exceeded size 1MB');
+		alert('Exceeded size 1MB');
 		return;
     }
     
 	let reader = new FileReader();
 	reader.addEventListener('loadstart', function() {
-	    console.log("Loading");
+		document.getElementById("word").innerHTML = "Loading...";
+		$("#word").fadeIn(100);
 	});
 
 	reader.addEventListener('load', function(e) {
-        let text = e.target.result;
-        let word = [], type = 1, wordCount = 0;
+		let text = e.target.result;
+		
+		const regex = /\s+/g;
+		words = text.split(regex);
 
-        for(let i=0; i<text.length+1; i++) {
-            if(text[i] == " " || text[i] == "\n" || i == text.length){
-                if(type == 1) {
-                    wordsQuest[wordCount] = word;
-                } else {
-                    wordsAnswer[wordCount] = word;
-                    wordCount++;
-                }
-
-                type *= -1;
-                word = "";
-            }
-            else word += text[i];
-        }
-
-        $("#fileInput").fadeOut(200);
-        gameMain();
+		if(words.length%2 == 0) {
+			$("#fileInput").fadeOut(200);
+			gameMain();
+		} else alert('Invalid number of words');
 	});
 
 	reader.addEventListener('error', function() {
-	    console.log('Error : Failed to read file');
+	    alert('Failed to read file');
 	});
 
 	reader.addEventListener('progress', function(e) {
 	    if(e.lengthComputable == true) {
-	    	console.log(Math.floor((e.loaded/e.total)*100));
+			progrssTo = Math.floor((e.loaded/e.total)*100);
         }
 	});
 	reader.readAsText(file);
 });
 
 function gameMain() {
-    console.log("ok");
+	wordRand();
+
+	document.getElementById("correctBar").style.display = "flex";
+
+	setInterval(correctBarUpdate, 2);
+	setInterval(progressUpdate, 2);
 }
+
+function wordRand() {
+	let sent = "";
+	let rand = wordNum;
+
+	while(rand == wordNum) wordNum = Math.floor(Math.random() * (words.length)/2)*2;
+
+	letterMis = "#2ecc71";
+	progressNow = progressTo = 0;
+
+	$("#sentence").fadeOut(100);
+
+	$("#word").fadeOut(100, function() {
+		document.getElementById("word").innerHTML = words[wordNum].replace(/_/g, " ");
+		document.getElementById("word").style.backgroundImage = 'linear-gradient(90deg, '+letterMis+' 0%, #fff 0%)';
+
+		for(let i = 0; i < words[wordNum+1].length; i++)sent += "<div class=\"letter\">"+words[wordNum+1][i]+"</div>";
+		document.getElementById("sentence").innerHTML = sent;
+
+		$("#word").fadeIn(100);
+		$("#sentence").fadeIn(100);
+	});
+}
+
+function progressUpdate() {
+	progressNow = progressNow + (progressTo-progressNow)*0.05;
+	document.getElementById("word").style.backgroundImage = 'linear-gradient(90deg, '+letterMis+' '+progressNow+'%, #fff 0%)';
+}
+
+function correctBarUpdate() {
+	let allQuest = mis + point;
+
+	if(allQuest != 0) {
+		correctBar = correctBar + ((point/allQuest*100)-correctBar)*0.1;
+		misBar = misBar + ((mis/allQuest*100)-misBar)*0.1;
+	}
+
+	document.getElementsByClassName("bar")[0].style.width = correctBar+"%";
+	document.getElementsByClassName("bar")[1].style.width = misBar+"%";
+
+	document.getElementsByClassName("bar")[0].innerHTML = Round(correctBar, 2)+"% ("+point+")";
+	document.getElementsByClassName("bar")[1].innerHTML = Round(misBar, 2)+"% ("+mis+")";
+
+	if(Math.round(correctBar) < 5) document.getElementsByClassName("bar")[0].innerHTML = "";
+	if(Math.round(misBar) < 5) document.getElementsByClassName("bar")[1].innerHTML = "";
+}
+
+function key(event) {
+	if(event.key != "Enter") {
+		if(wordInput.length != words[wordNum+1].length) {
+			wordInput = wordInput + event.key.toLowerCase();
+
+			if(wordInput[wordInput.length-1] != words[wordNum+1][wordInput.length-1]) {
+				letterMis = "#e74c3c";
+				document.getElementsByClassName("letter")[wordInput.length-1].style.borderBottom = "2px solid #e74c3c";
+				document.getElementsByClassName("letter")[wordInput.length-1].style.color = "#e74c3c";
+				document.getElementsByClassName("letter")[wordInput.length-1].innerHTML = wordInput[wordInput.length-1];
+			} else {
+				document.getElementsByClassName("letter")[wordInput.length-1].style.borderBottom = "2px solid #2ecc71";
+				document.getElementsByClassName("letter")[wordInput.length-1].style.color = "#2ecc71";
+			}
+
+			progressTo = Math.round((wordInput.length/words[wordNum+1].length*100));
+			progressUpdate();
+
+			if(wordInput.length >= words[wordNum+1].length) {
+				if(wordInput == words[wordNum+1]) point++;
+				else mis++;
+			}
+		}
+		if(wordInput.length == words[wordNum+1].length) {
+			$("#word").fadeOut(100, function() {
+				document.getElementById("word").innerHTML = words[wordNum+1].replace(/_/g, " ");
+				$("#word").fadeIn(100);
+			});
+		} 
+	} else if(event.key == "Enter") {
+		if((wordInput != words[wordNum+1]) && (wordInput.length != words[wordNum+1].length)) mis++;
+		wordInput = "";
+		wordRand();
+	}
+}
+
+function Round(n, k) {
+    let factor = Math.pow(10, k);
+    return Math.round(n*factor)/factor;
+}
+
+document.addEventListener("keypress", key, false);
